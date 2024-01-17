d466bc28d15008717c1197117d3cd4ba55eee503
Jorge Siam <149221268+Jorgesiam96@users.noreply.github.com>
2024-01-17 04:27:47 -0300

    DevSorteio

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+function sorte(){
+var input1 = Math.ceil(document.querySelector(".input-1").value)
+var input2 = Math.floor(document.querySelector(".input-2").value)
+    
+var result = Math.floor(Math.random()*(input1-input2 + 1)) + input2;
+
+alert(result)
+
+
+}
