699512ffaef7189ec2381e250e48bbac3399580d
Jorge Siam <149221268+Jorgesiam96@users.noreply.github.com>
2024-02-02 00:31:53 -0300

    settimeout

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 
-function sorte(){
-var input1 = Math.ceil(document.querySelector(".input-1").value)
-var input2 = Math.floor(document.querySelector(".input-2").value)
-    
-var result = Math.floor(Math.random()*(input1-input2 + 1)) + input2;
+setTimeout (function sorte() {
+    var input1 = Math.ceil(document.querySelector(".input-1").value)
+    var input2 = Math.floor(document.querySelector(".input-2").value)
 
-alert(result)
+    var result = Math.floor(Math.random() * (input1 - input2 + 1)) + input2;
 
 
-}
+
+    if (input2 < input1) {
+        alert(result)
+    }
+    else {
+        alert("O valor MIN tem que ser maior que o valor MAX")
+    }
+} ,timeout)
